1c4e132a444b9246a639f56487faae1207596c8d
Brian Han <shan790@aucklanduni.ac.nz>
2024-05-09 16:58:29 +1200

    Added tracker for number of rounds, and passed 2 more test cases

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,12 +6,20 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private int currentRound = 0;
+  private String playerName;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    this.playerName = options[0];
   }
 
-  public void play() {}
+  public void play() {
+    currentRound++;
+    MessageCli.START_ROUND.printMessage(Integer.toString(currentRound));
+    MessageCli.ASK_INPUT.printMessage();
+  }
 
   public void endGame() {}
 
